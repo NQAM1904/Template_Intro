a51b1c929bea80fb716165434dfa74a3829ddf1a
Minh <nghiemminh1904@gmail.com>
2021-02-19 16:05:49 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Feature from './Components/Feature';
 import Header from './Components/Header';
+import About from './Components/About';
+import aboutimage from './images/Frame 19.png'
+import aboutimage1 from './images/download.png'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Feature />
+      <About
+        image={aboutimage}
+        title='Comes With All You Need For Daily Life'
+        button='Get The App'
+      />
+      <About
+        image={aboutimage1}
+        title='Download And Get The App'
+        button='Download'
+      />
     </div>
   );
 }
